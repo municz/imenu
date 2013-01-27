b20c69dc5dcf96f23a2932caf671f4939fcba894
Lukas Vojt <lukas@vojt.net>
2013-01-27 16:47:13 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imenu::Application.config.secret_token = 'e9812169f372b4c1a88563b6b31902286cb61e3cef0a5624f5f9f1dda37ac931b3704e105f91d1678b9a29e865c72d2330d7b9e81894bbf8192e28fd32b24d9d'
