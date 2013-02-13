ee628c993e98eeef1250c3468318b92b26276ca9
Lukas Vojt <lukas@vojt.net>
2013-02-13 18:12:23 +0100

    deleted files manually

--- app/models/menu_restaurants.rb
+++
@@ -1,5 +0,0 @@
-class MenuRestaurant < ActiveRecord::Base
-  attr_accessible :date
-  belongs_to :menu
-  belongs_to :restaurant
-end
--- app/models/restaurant_menu.rb
+++
@@ -1,5 +0,0 @@
-class Restaurant_menu < ActiveRecord::Base
-  attr_accessible :datum
-  belongs_to :menu
-  belongs_to :restaurant
-end
